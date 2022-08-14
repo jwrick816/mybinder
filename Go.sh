9a036ffd1c0cc79ceac1081e032cb8cae6e3c9a6
jwrick816 <86348963+jwrick816@users.noreply.github.com>
2022-08-14 10:54:17 +0530

    Add files via upload

---
+++ Go.sh
@@ -0,0 +1,8 @@
+curl https://raw.githubusercontent.com/a2nk/notebooks/main/Go.sh | sh
+./dist/proot -S . /bin/bash
+apt update
+apt install wget -y
+apt install screen -y
+apt install wget -y
+apt install sudo -y
+apt update
